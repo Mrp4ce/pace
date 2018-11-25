98885c870c209e3459a1662c43ced442be7a4deb
Mrp4ce <41745268+Mrp4ce@users.noreply.github.com>
2018-11-25 09:52:08 +0700

    Add files via upload
    Menu project menu makanan dengan python2

---
+++ menu.py
@@ -0,0 +1,58 @@
+#!  /usr/bin/python
+print ("\n")
+print ("\n")
+# Percobaan dalam menggunakan python
+print("####  ///////P P P P PP.      ***********")
+print ("###  /////// .   .  ..p.     *****      ")
+print ("##  ///////   p p p p     . *****")
+print ("#  ///////.     .          *************")
+text ="blue" 
+print ("\n")
+print ("###############################\n")
+nama = raw_input('Masukan Nama Anda : ')
+print "(+)SELAMAT DATANG (*)  " , nama
+passw  = raw_input('(+)Masukan Password Anda : ' )
+alamat = raw_input ('(+)Masukan Alamat Anda : ')
+print ("////////////////////////////////\n")
+i = "          ++rendang"
+a ="          ++rawon"
+b ="          ++gulai ayam"
+c="          ++ikan bakar"
+d="          ++ayam goreng"
+e="          ++mie goreng "
+f="          ++sea food"
+h="          ++udang bakar keju"
+j="          ++nasi pecel"
+k="          ++gado-gado"
+l="          ++kerak telor"
+g =" ($)Menu masakan hari ini : "
+fg ="blue"
+pw = "pace"
+if passw == pw:
+	print "(+)Selamat Datang Kembali ", nama
+	print g
+	print a
+	print c
+	print d
+	print i
+	print e
+	
+	x = "Terima Kasih ! Kami akan antar ke tempat anda", alamat
+	menu = raw_input(' ($) mau makan apa hari ini . :')
+	if menu == 'rendang':
+		 print" ($) Harga 15.000,00 !!   \n(*)Kami Akan Antar Ke tempat Anda (=)" , alamat
+         
+	elif menu == 'rawon':
+		  print "harga = Rp. 17.000,00  \n(*)Kami Akan Antar Ke tempat Anda (=)" , alamat
+	elif menu == 'ayam bakar':
+		  print "harga = Rp. 20.000,00  \n(*)Kami Akan Antar Ke tempat Anda (=)" , alamat
+	elif menu == 'mie goreng':
+		  print "harga = Rp. 7.000,00  \n(*)Kami Akan Antar Ke tempat Anda (=)" , alamat
+
+	elif menu == 'ikan bakar':
+		  print "harga = Rp.16.000,00 \n(*)Kami Akan Antar Ke tempat Anda (=)" , alamat
+else:
+	print "(+)Kamu bukan Pace .. Minta Password Sono :v \n becanda ahh , password = (pace) : " 
+	print "(+) Copyright goblokk :V "
+
+	
